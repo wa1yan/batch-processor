b1813cc75dfa185880188bd582cf898bd945e713
wa1yan <waiyanucsy1@gmail.com>
2024-09-08 01:12:45 +0630

    updated: security config

--- src/main/java/dev/waiyanhtet/batch_processor/config/SecurityConfig.java
+++ src/main/java/dev/waiyanhtet/batch_processor/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package dev.waiyanhtet.batch_processor.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,13 +13,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeHttpRequests(authorizedRequest ->
+        http.authorizeHttpRequests(authorizedRequest ->
                         authorizedRequest
                                 .requestMatchers("/api/v1/greet").permitAll()
-                                .anyRequest().authenticated()
-                ).oauth2ResourceServer().jwt();
+                                .anyRequest().authenticated());
+
+//        we can use opaqueToken also to validate token
+//        http.oauth2ResourceServer(server ->
+//                server.opaqueToken(
+//                        Customizer.withDefaults()
+//                )
+//        );
+
+        http.oauth2ResourceServer(server ->
+                        server.jwt(
+                                Customizer.withDefaults()
+                        ));
 
         return http.build();
     }
